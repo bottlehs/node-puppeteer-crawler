a193cb0a8f4565af50390b132fcd43e49005aeaf
bottlehs <qudgns9@gmail.com>
2021-04-17 21:15:58 +0900

    #7 LogsAdapter 추가

---
+++ app2/app/src/main/java/web/macro/app/LogsAdapter.kt
@@ -0,0 +1,68 @@
+package web.macro.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclersample.R
+import com.example.recyclersample.data.Flower
+
+class LogsAdapter(private val onClick: (Flower) -> Unit) :
+    ListAdapter<Flower, FlowersAdapter.FlowerViewHolder>(FlowerDiffCallback) {
+
+    /* ViewHolder for Flower, takes in the inflated view and the onClick behavior. */
+    class FlowerViewHolder(itemView: View, val onClick: (Flower) -> Unit) :
+        RecyclerView.ViewHolder(itemView) {
+        private val flowerTextView: TextView = itemView.findViewById(R.id.flower_text)
+        private val flowerImageView: ImageView = itemView.findViewById(R.id.flower_image)
+        private var currentFlower: Flower? = null
+
+        init {
+            itemView.setOnClickListener {
+                currentFlower?.let {
+                    onClick(it)
+                }
+            }
+        }
+
+        /* Bind flower name and image. */
+        fun bind(flower: Flower) {
+            currentFlower = flower
+
+            flowerTextView.text = flower.name
+            if (flower.image != null) {
+                flowerImageView.setImageResource(flower.image)
+            } else {
+                flowerImageView.setImageResource(R.drawable.rose)
+            }
+        }
+    }
+
+    /* Creates and inflates view and return FlowerViewHolder. */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FlowerViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.flower_item, parent, false)
+        return FlowerViewHolder(view, onClick)
+    }
+
+    /* Gets current flower and uses it to bind view. */
+    override fun onBindViewHolder(holder: FlowerViewHolder, position: Int) {
+        val flower = getItem(position)
+        holder.bind(flower)
+
+    }
+}
+
+object FlowerDiffCallback : DiffUtil.ItemCallback<Flower>() {
+    override fun areItemsTheSame(oldItem: Flower, newItem: Flower): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Flower, newItem: Flower): Boolean {
+        return oldItem.id == newItem.id
+    }
+}
