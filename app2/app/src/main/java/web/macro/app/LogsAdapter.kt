be7d4b9585c9dfb75aacc9d9b0bfa136aad5ce33
bottlehs <qudgns9@gmail.com>
2021-04-25 19:34:12 +0900

    #7 LogsAdapter

--- app2/app/src/main/java/web/macro/app/LogsAdapter.kt
+++ app2/app/src/main/java/web/macro/app/LogsAdapter.kt
@@ -1,21 +1,43 @@
 package web.macro.app
 
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.log_item.view.*
 
-class LogsAdapter (private val items: ArrayList<DataLogs>) : RecyclerView.Adapter<RecyclerAdapterLogs.ViewHolder>() {
+class LogsAdapter (private val items: ArrayList<DataLogs>) : RecyclerView.Adapter<LogsAdapter.ViewHolder>() {
+
     override fun getItemCount(): Int = items.size
 
-    override fun onBindViewHolder(holder: RecyclerAdapterLogs.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: LogsAdapter.ViewHolder, position: Int) {
+        val item = items[position]
+        val listener = View.OnClickListener { it ->
+            Toast.makeText(it.context, "Clicked"+item.strProduct, Toast.LENGTH_SHORT).show()
+        }
+        holder.apply {
+            bind(listener, item)
+            itemView.tag = item
+        }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): RecyclerAdapterLogs.ViewHolder {
+    ): LogsAdapter.ViewHolder {
+        TODO("Not yet implemented")
+        val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.log_item, parent, false)
+
+        return LogsAdapter.ViewHolder(inflatedView)
     }
 
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-
+        private var view: View = v
+        fun bind(listener: View.OnClickListener, item: DataLogs) {
+            view.date.text = "date";
+            view.product.text = "product";
+            view.purchase.text = "purchase";
+        }
     }
 }
