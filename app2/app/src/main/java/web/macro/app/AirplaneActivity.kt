5afd9bfbdd73af20df94972f8faa38983152b76d
bottlehs <qudgns9@gmail.com>
2021-05-11 19:32:17 +0900

    airplane mode

---
+++ app2/app/src/main/java/web/macro/app/AirplaneActivity.kt
@@ -0,0 +1,93 @@
+package web.macro.app
+
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.annotation.TargetApi
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.view.accessibility.AccessibilityManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_log.*
+
+private const val PERMISSION_CODE = 110
+
+class AirplaneActivity: AppCompatActivity() {
+    private val TAG = AirplaneActivity::class.qualifiedName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_airplane)
+
+        // set toolbar as support action bar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.apply {
+            title = getString(R.string.activity_airplane_title)
+
+            // show back button on toolbar
+            // on back button press, it will navigate to parent activity
+            setDisplayHomeAsUpEnabled(true)
+            setDisplayShowHomeEnabled(true)
+        }
+    }
+
+    private fun checkAccess(): Boolean {
+        val string = getString(R.string.accessibility_service_id)
+        val manager = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+        val list = manager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC)
+        for (id in list) {
+            if (string == id.id) {
+                return true
+            }
+        }
+        return false
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val hasPermission = checkAccess()
+        "has access? $hasPermission".logd()
+        if (!hasPermission) {
+            startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+            && !Settings.canDrawOverlays(this)) {
+            askPermission()
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun askPermission() {
+        val intent = Intent(
+            Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+            Uri.parse("package:$packageName"))
+        startActivityForResult(intent, PERMISSION_CODE)
+    }
+
+    override fun onDestroy() {
+        serviceIntent?.let {
+            "stop floating click service".logd()
+            stopService(it)
+        }
+        autoClickService?.let {
+            "stop auto click service".logd()
+            it.stopSelf()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) return it.disableSelf()
+            autoClickService = null
+        }
+        super.onDestroy()
+    }
+
+    override fun onBackPressed() {
+        moveTaskToBack(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+}
