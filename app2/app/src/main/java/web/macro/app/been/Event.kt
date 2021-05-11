f65b1d6d07bcdf05fcfeecb89e7077f6bd187c81
bottlehs <qudgns9@gmail.com>
2021-05-11 19:32:25 +0900

    event

---
+++ app2/app/src/main/java/web/macro/app/been/Event.kt
@@ -0,0 +1,37 @@
+package web.macro.app.been
+
+import android.accessibilityservice.GestureDescription
+import android.graphics.Path
+import android.graphics.Point
+
+abstract class Event {
+    var startTime = 10L
+    var duration = 10L
+    lateinit var path: Path
+    fun onEvent(): GestureDescription.StrokeDescription {
+        path = Path()
+        movePath()
+        return GestureDescription.StrokeDescription(path, startTime, duration)
+    }
+
+    abstract fun movePath()
+}
+
+data class Move(val to: Point) : Event() {
+    override fun movePath() {
+        path.moveTo(to.x.toFloat(), to.y.toFloat())
+    }
+}
+
+data class Click(val to: Point) : Event() {
+    override fun movePath() {
+        path.moveTo(to.x.toFloat(), to.y.toFloat())
+    }
+}
+
+data class Swipe(val from: Point, val to: Point) : Event() {
+    override fun movePath() {
+        path.moveTo(from.x.toFloat(), from.y.toFloat())
+        path.lineTo(to.x.toFloat(), to.y.toFloat())
+    }
+}
