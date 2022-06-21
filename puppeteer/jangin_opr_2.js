60e5299d0d13170e6c15d32cca7055fc7df21f35
bottlehs <qudgns9@gmail.com>
2022-06-21 13:53:47 +0900

    크롤링 오류 수정

--- puppeteer/jangin_opr_2.js
+++ puppeteer/jangin_opr_2.js
@@ -7,7 +7,7 @@ let page = 2;
 let max_page = 3;
 let items = [];
 let complteItems = [];
-let startDate = "2022-01-07";
+let startDate = "2022-03-01";
 let endDate = "2022-01-07";
 let limitEndDate = "";
 
